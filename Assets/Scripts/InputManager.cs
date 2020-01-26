422cd163a58bed1dace092c68bd0459d7f459b44
ashishgopalhattimare <ashishgopalhattimare@gmail.com>
2020-01-26 12:23:33 +0530

    Update InputManager.cs
    comment code

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,60 +5,58 @@ using Vectrosity;
 
 public class InputManager : MonoBehaviour {
 
-	public static int MAX_LEN = 1000;
+	public static int MAX_LEN = 1000;		// Maximum length of the pathArray
 
-	public GameObject arrowGameObject;
-	// Use this for initialization
+	public GameObject arrowGameObject;		// Original instance of the arrow figure
 
-	private Vector3[] hitArr;		// default path setter
-	private Vector3[] path1Array,	// path1 
-					  path2Array;	// path2
+	private Vector3[] hitArr;				// store the points of the current path
+	private Vector3[] path1Array,			// store source to destination path of the location A
+					  path2Array;			// store source to destination path of the location B
 
-	private GameObject[] gameArray;
+	private GameObject[] gameArray;			// store the drawable figure of the arrow on the flat plane
 
-	private int i;					// Number of vector points in the path
-	private int len, k;				// path variables
+	private int i;							// Number of vector points in the current pathArray
+	private int len, k;						// totalLength of the pathArray to be iterated or stored in location X
 
-	private int path1Len, path2Len;
+	private int path1Len, path2Len;			// length of the path of location A and B, respectively
 	
-	public float speed;
+	public float speed;						// manage the speed of the helper rabit
 
-	public GameObject followPrefab;
-	private GameObject travellingSalesman;
+	public GameObject followPrefab;			// the original figure of the rabit
+	private GameObject travellingSalesman;	// replicate the figure of the rabit for the application
 
-	private int followPathBool;
+	private int followPathBool;				// whether the path is to followed or not
 
-	public static bool rabitVisible;
+	public static bool rabitVisible;		// whether the rabit is visible on the flat plane or not
 
-	void Start () {
+	void Start ()
+	{
 		initialize();
 
-		path1Array = new Vector3[MAX_LEN];
-		path2Array = new Vector3[MAX_LEN];
+		path1Array = new Vector3[MAX_LEN];		// create an array for the path1Array
+		path2Array = new Vector3[MAX_LEN];		// create an array for the path2Array
 
-		gameArray  = new GameObject[MAX_LEN];
+		gameArray  = new GameObject[MAX_LEN];	// create an array of the arrow figure
 
-		path1Len = 0; path2Len = 0;
+		path1Len = 0; path2Len = 0;				// initialize the pathLen to 0
 
-		rabitVisible = false; // initial not visible state
+		rabitVisible = false; 					// initial not visible state
 	}
 
-	public void initialize() {
-
-		hitArr = new Vector3[MAX_LEN];
-		i = 0;
-		len = -1;
-		followPathBool = 0;
-		speed = 0.55f;
+	public void initialize()
+	{
+		hitArr = new Vector3[MAX_LEN];	// reset the current pathArray for the current path
+		i = 0;							// set pathArray length to 0
+		len = -1;						// set final pathArray length to -1 (for safety purposes)
+		followPathBool = 0;				// set followPath Boolean to false, initially
+		speed = 0.55f;					// set the speed of the helper rabit
 	}
 	
-	// Update is called once per frame
-	void Update () {
-
+	void Update ()
+	{
 		if(Input.GetMouseButtonDown(0) && followPathBool == 0)
 		{
 			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
 			RaycastHit hitInfo;
 
 			if(Physics.Raycast(ray , out hitInfo))
@@ -72,7 +70,6 @@ public class InputManager : MonoBehaviour {
 			}
 		}
 
-		// When follow button is pressed
 		if(followPathBool == 1)
 		{
 			float step = speed * Time.deltaTime;
